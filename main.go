d8303006863e7a7df84793f106d8e49672578f4d
SilvaVibe <igor.juan108@gmail.com>
2025-04-03 10:00:01 -0300

    Vetores 1

--- main.go
+++ main.go
@@ -4,6 +4,13 @@ import "fmt"
 
 func main() {
 
+	//Slice
+	numeros := []int{1, 2, 3, 4, 5}
+	fmt.Println(numeros)
+	numeros = append(numeros, 6, 7, 8)
+	fmt.Println(numeros, len(numeros), cap(numeros))
+	
+
 	// Slice
 	var nomes = []string{"Igor", "Silva", "Dusk", "Robert"}
 	fmt.Println(nomes)
